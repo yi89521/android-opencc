2b6710e4b06ac2ec0815520bb8dce58d1c46affc
sean <sean@gopaktor.com>
2016-03-01 17:47:43 +0800

    use standard c++ lib for compilation

--- lib-opencc-android/src/main/jni/chineseconverter.cpp
+++ lib-opencc-android/src/main/jni/chineseconverter.cpp
@@ -1,6 +1,9 @@
 #include <jni.h>
 #include "opencc/Converter.hpp"
+#include "opencc/Config.hpp"
 
+opencc::ConverterPtr converter;
+opencc::Config config;
 
 extern "C"
 jstring
@@ -9,7 +12,8 @@ Java_com_zqc_opencc_android_lib_ChineseConverter_convert(JNIEnv *env, jobject in
     const char *text = env->GetStringUTFChars(text_, 0);
     const char *type = env->GetStringUTFChars(type_, 0);
 
-    const opencc::ConverterPtr converter;
+    const string name("s2hk.json");
+    converter = config.NewFromFile(name);
     std::string string1 = converter->Convert(text);
 
     return  env->NewStringUTF(string1.c_str());
